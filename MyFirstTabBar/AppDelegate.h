49c3ffa148b3daf73947c6694e9a38518887f788
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-11-01 18:22:11 -0600

    Initial Commit

---
+++ MyFirstTabBar/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyFirstTabBar
+//
+//  Created by Marco Antonio Roa De La Cruz on 11/1/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MyFirstTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstTabBar
+//
+//  Created by Marco Antonio Roa De La Cruz on 11/1/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
